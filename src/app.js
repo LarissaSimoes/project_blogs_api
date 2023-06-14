0f2ef8bdfd32a427367c5d7072ad9a8fa6c0779f
Larissa Simões <larissacarneiro2@hotmail.com>
2023-06-14 14:58:29 -0300

    Req 1 em andamento

--- src/app.js
+++ src/app.js
@@ -16,5 +16,3 @@ app.use(express.json());
 // É importante exportar a constante `app`,
 // para que possa ser utilizada pelo arquivo `src/server.js`
 module.exports = app;
-
-// Commit
---
+++ src/migrations/20230614163725-blog_posts.js
@@ -0,0 +1,43 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('blog_posts', {
+      id: {
+        allowNull:false,
+        autoIncrement:true,
+        primaryKey:true,
+        type:Sequelize.INTEGER
+      },
+      title: {
+        allowNull:false,
+        type:Sequelize.STRING
+      },
+      content: {
+        allowNull:false,
+        type:Sequelize.STRING
+      },
+      user_id: {
+        allowNull:false,
+        type:Sequelize.INTEGER,
+        foreignKey: true,
+        references: {
+          model: 'users',
+          key: 'id'
+        }
+      },
+      published: {
+        allowNull:false,
+        type:Sequelize.DATE
+      },
+      updated: {
+        allowNull:false,
+        type:Sequelize.DATE
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+   await queryInterface.dropTable('blog_posts');
+  }
+};
