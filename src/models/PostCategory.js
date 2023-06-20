e2f10e3c0d6702569b244b1d94a96d6e752cfb11
Larissa Simões <larissacarneiro2@hotmail.com>
2023-06-19 21:50:47 -0300

    Req 11

---
+++ src/models/PostCategory.js
@@ -0,0 +1,29 @@
+module.exports = (sequelize, DataTypes) => {
+    const postCategoryTable = sequelize.define('PostCategory', {
+      postId: DataTypes.INTEGER,
+      categoryId: DataTypes.INTEGER,
+    }, {
+      tablename: 'posts_categories',
+      underscored: true,
+      timestamps: false,
+    })
+
+    postCategoryTable.associate = ({ BlogPost, Category }) => {
+
+        BlogPost.belongsToMany(Category, {
+          through: postCategoryTable,
+          foreignKey: 'postId',
+          otherKey: 'categoryId'
+        });
+    
+        Category.belongsToMany(BlogPost, {
+          through: postCategoryTable,
+          foreignKey: 'categoryId',
+          otherKey: 'postId' 
+        });
+    
+      }
+    
+ 
+    return postCategoryTable;
+  };
